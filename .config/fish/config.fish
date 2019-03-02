fddc06f584a0b5b2b09a347a908dbd70fd8476fd
barbafh <barbafh@gmail.com>
2019-03-02 17:25:41 -0300

    Moved scripts into descriptive folders and changed path folder to ~/bin with symlinks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,9 +6,6 @@ fish_vi_key_bindings
 # Load the abbreviations file
 source $HOME/.config/fish/abbreviations
 
-set PATH ~/.scripts $PATH
-set PATH ~/.scripts/gen-aliases $PATH
+set PATH ~/bin $PATH
 set PATH /snap/bin $PATH
 set PATH ~/.local/bin $PATH
-
-blurk
