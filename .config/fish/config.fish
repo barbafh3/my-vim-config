08d8cdf6d54725ef40677b8699e9f2ab69f11ba0
barbafh <barbafh@gmail.com>
2019-03-02 21:30:34 -0300

    Added fzf script and aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,8 @@ fish_vi_key_bindings
 # Load the abbreviations file
 source $HOME/.config/fish/abbreviations
 
+set EDITOR /usr/bin/vim
+
 set PATH ~/.scripts/bin $PATH
 set PATH /snap/bin $PATH
 set PATH ~/.local/bin $PATH
