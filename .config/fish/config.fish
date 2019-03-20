59b95c1791e4fdef519c53d03515be567c7c2468
barbafh <barbafh@gmail.com>
2019-03-20 18:24:50 -0300

    Added pywal color loading for xfce4-terminal

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ fish_vi_key_bindings
 
 # Load the abbreviations file
 source $HOME/.config/fish/abbreviations
+cat ~/.cache/wal/sequences
 
 set EDITOR /usr/bin/vim
 set JAVA_HOME /usr/lib/jvm/java-11-openjdk/bin
