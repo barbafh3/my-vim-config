58695ac7726290e87e5427c8ba9cee9fb21aa530
Gilberto Timotheo Junior <barbafh@gmail.com>
2020-02-20 00:02:58 -0300

    Added web script global variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,19 +9,13 @@ source $HOME/.config/fish/abbreviations
 
 set fish_greeting ''
 
-set EDITOR /usr/bin/vim $EDITOR
 set JAVA_HOME /usr/lib/jvm/java-11-openjdk/bin $JAVA_HOME
 set MSBuildSDKsPath /usr/share/dotnet/sdk/(dotnet --version) $MSBuildSDKsPath
 # set FrameworkPathOverride /lib/mono/4.5
 
-set PATH ~/.scripts/bin $PATH
-set PATH /snap/bin $PATH
 set PATH ~/.local/bin $PATH
 set PATH ~/.gem/ruby/2.6.0/bin $PATH
 set PATH /home/manjaro/.dotnet/tools $PATH
-
-set TERMINAL /usr/bin/kitty $TERMINAL
-set IMAGE ~/Imagens/wallpapers/Mountain-1080p-wallpaper.jpg
 
 powerline-daemon -q
 
--- .profile
+++ .profile
@@ -2,12 +2,15 @@ export QT_QPA_PLATFORMTHEME="qt5ct"
 export XDG_CONFIG_HOME="$HOME/.config"
 export PATH=$PATH:~/.scripts/bin
 export PATH=$PATH:~/bin
+export PATH=$PATH:/snap/bin
+export PATH=$PATH:~/.local/bin
 export SXHKD_SHELL=/bin/bash
+export GTK_THEME="Adwaita-dark"
 
 export EDITOR=/usr/bin/nvim
 export TERMINAL=/usr/bin/kitty
+export BROWSER=/usr/bin/qutebrowser
 
 export TWITCH_FAVORITES=~/Documentos/twitch-favorites
 export BROWSER_FAVORITES=~/Documentos/browser-favorites
 export BROWSER_MOST_USED=~/Documentos/browser-most-used
-export GTK_THEME="Adwaita-dark"
